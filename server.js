290af85a04205f091d777de295b5d1ec4fc8dc1a
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-10-04 17:01:44 -0700

    debugging heroku branch

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+const server = require('http').Server(app);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/traffic-citation-frontend'));
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/traffic-citation-frontend/index.html'));
-});
+server.listen(port, () => 
+    console.log("App running on port " + port),
+);
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) => 
+    res.sendFile(path.join(__dirname, 'src', 'index.html')),
+);
+
