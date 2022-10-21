b9e525769186e0b37d2eae3020c49e0886f4d510
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-10-21 12:56:34 -0700

    create citation now supports adding more than one violation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://traffic-citation-backend.herokuapp.com/api",
-  //apiUrl: "http://localhost:7190/api"
+  //apiUrl: "https://traffic-citation-backend.herokuapp.com/api",
+  apiUrl: "http://localhost:7190/api"
 };
