7b3664b0e736d78294201f212d7137c43654823b
Alfredo Hernandez <80275985+ahernandez1215@users.noreply.github.com>
2022-11-09 12:44:13 -0800

    Summary now shows information

--- src/app/components/citations/view-citation-summary/view-citation-summary.component.ts
+++ src/app/components/citations/view-citation-summary/view-citation-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Citation } from 'src/app/models/citation';
 import { Driver } from 'src/app/models/driver';
 import { Violation } from 'src/app/models/violation';
@@ -13,33 +13,30 @@ export class ViewCitationSummaryComponent
   extends Unsubscriber
   implements OnInit
 {
-  citation?: Citation;
-  violations?: Violation[];
-  driver?: Driver;
-
-  // TODO: Use local storage instead of session to store data in browser cache
+  @Input() citation?: Citation;
+  @Input() violations?: Violation[];
+  @Input() driver?: Driver;
 
   constructor() {
     super();
   }
 
   ngOnInit(): void {
-    this.retrieveSessionValues();
   }
 
-  retrieveSessionValues() {
-    // Parse JSON string from session storage as Citation
-    if (sessionStorage.getItem('citation')) {
-      this.citation = JSON.parse(
-        sessionStorage.getItem('citation') || '{}') as Citation;
-    }
+  // retrieveSessionValues() {
+  //   // Parse JSON string from session storage as Citation
+  //   if (sessionStorage.getItem('citation')) {
+  //     this.citation = JSON.parse(
+  //       sessionStorage.getItem('citation') || '{}') as Citation;
+  //   }
 
-    if (sessionStorage.getItem('violations')) {
-      this.violations = JSON.parse(sessionStorage.getItem('violations') || '{}') as Violation[];
-    }
+  //   if (sessionStorage.getItem('violations')) {
+  //     this.violations = JSON.parse(sessionStorage.getItem('violations') || '{}') as Violation[];
+  //   }
 
-    if (sessionStorage.getItem('driver')) {
-      this.driver = JSON.parse(sessionStorage.getItem('driver') || '{}') as Driver;
-    }
-  }
+  //   if (sessionStorage.getItem('driver')) {
+  //     this.driver = JSON.parse(sessionStorage.getItem('driver') || '{}') as Driver;
+  //   }
+  // }
 }
